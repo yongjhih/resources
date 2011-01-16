642164db666c07ea9d90a66e1a953d49c6d9ed8f
DeepT <deept-mods@hotmail.com>
2011-01-17 04:00:06 +0800

    [zombies] Add Zombie speed now depend on different zombie type

--- zombies/zombie_server.lua
+++ zombies/zombie_server.lua
@@ -2,6 +2,8 @@ ZombieLimit = get("zombies.MaxZombies")-- HOW MANY ZOMBIES SHOULD EXIST AT MAXIM
 ZombieStreaming = get("zombies.StreamMethod") -- 1 to constantly stream zombies, 0 to only allow zombies to spawn via createZombie function, 2 to only allow spawning at set spawnpoints
 ZombiePedSkins = {13,22,56,67,68,69,70,92,97,105,107,108,126,127,128,152,162,167,188,195,206,209,212,229,230,258,264,277,280,287 } --ALTERNATE SKIN LISTS FOR ZOMBIES (SHORTER LIST IS TEXTURED ZOMBIES ONLY)
 --ZombiePedSkins = {7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,43,44,45,36,37,38,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,66,67,68,69,70,71,72,73,75,76,77,78,79,80,81,82,83,85,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,112,113,114,115,116,117,118,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,150,151,152,153,154,155,156,157,158,159,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,209,210,211,212,213,214,215,216,217,218,219,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255,256,257,258,259,260,261,262,263,264,275,276,277,278,279,280,281,282,283,284,285,286,287,288 }
+fastZombies = { 264,277,287 }
+slowZombies = { 13,22,56 }
 ZombieSpeed = get("zombies.Speed")
 if ZombieSpeed == 0 then --super slow zombies (goofy looking)
 	chaseanim = "WALK_drunk"
@@ -45,6 +47,7 @@ end
 --BEHAVIOUR WHILE CHASING PLAYERS
 function Zomb_chase (ped, Zx, Zy, Zz )
 	if isElement(ped) then
+	local zskin = getElementModel ( ped )
 		if (getElementData ( ped, "status" ) == "chasing") and (getElementData (ped, "zombie") == true) then
 			local x, y, z = getElementPosition( ped )
 			if (getElementData ( ped, "target" ) == nil) and getElementData ( ped, "Tx" ) ~= false then			
@@ -67,6 +70,17 @@ function Zomb_chase (ped, Zx, Zy, Zz )
 						if action == 1 then
 							setPedAnimation ( ped )
 							triggerClientEvent ( "Zomb_Punch", getRootElement(), ped )
+							chaseanim = "run_old"
+							for k, skin in pairs( slowZombies ) do
+								if skin == zskin then
+									chaseanim = "WALK_drunk"
+								end
+							end
+							for k, skin in pairs( fastZombies ) do
+								if skin == zskin then
+									chaseanim = "Run_Wuzi"
+								end
+							end
 							setTimer ( function (ped) if ( isElement ( ped ) ) then setPedAnimation ( ped, "ped", chaseanim, -1, true, true, true ) end end, 800, 1, ped )
 							setTimer ( Zomb_chase, 2000, 1, ped, x, y, z )
 						elseif action == 2 then
@@ -96,6 +110,17 @@ function Zomb_chase (ped, Zx, Zy, Zz )
 								else
 									setPedAnimation ( ped)
 									triggerClientEvent ( "Zomb_Punch", getRootElement(), ped )
+									chaseanim = "run_old"
+									for k, skin in pairs( slowZombies ) do
+										if skin == zskin then
+											chaseanim = "WALK_drunk"
+										end
+									end
+									for k, skin in pairs( fastZombies ) do
+										if skin == zskin then
+											chaseanim = "Run_Wuzi"
+										end
+									end
 									setTimer ( function (ped) if ( isElement ( ped ) ) then setPedAnimation ( ped, "ped", chaseanim, -1, true, true, true ) end end, 800, 1, ped )
 									setTimer ( Zomb_chase, 2000, 1, ped, x, y, z )
 								end
@@ -109,6 +134,17 @@ function Zomb_chase (ped, Zx, Zy, Zz )
 								if action == 1 then
 									setPedAnimation ( ped)
 									triggerClientEvent ( "Zomb_Punch", getRootElement(), ped )
+									chaseanim = "run_old"
+									for k, skin in pairs( slowZombies ) do
+										if skin == zskin then
+											chaseanim = "WALK_drunk"
+										end
+									end
+									for k, skin in pairs( fastZombies ) do
+										if skin == zskin then
+											chaseanim = "Run_Wuzi"
+										end
+									end
 									setTimer ( function (ped) if ( isElement ( ped ) ) then setPedAnimation ( ped, "ped", chaseanim, -1, true, true, true ) end end, 800, 1, ped )
 									setTimer ( Zomb_chase, 2000, 1, ped, x, y, z )
 								elseif action == 2 then
@@ -123,6 +159,20 @@ function Zomb_chase (ped, Zx, Zy, Zz )
 					end
 				end
 			else
+				chaseanim = "run_old"
+				checkspeed = 1000
+				for k, skin in pairs( slowZombies ) do
+					if skin == zskin then
+						chaseanim = "WALK_drunk"
+						checkspeed = 2000
+					end
+				end
+				for k, skin in pairs( fastZombies ) do
+					if skin == zskin then
+						chaseanim = "Run_Wuzi"
+						checkspeed = 680
+					end
+				end
 				setPedAnimation ( ped, "ped", chaseanim, -1, true, true, true) --KEEP WALKING
 				setTimer ( Zomb_chase, checkspeed, 1, ped, x, y, z ) --CHECK AGAIN
 			end
@@ -409,6 +459,21 @@ function RanSpawn_Z ( gx, gy, gz, rot)
 				setElementData ( zomb, "zombie", true  )
 				table.insert( everyZombie, zomb )	
 				setTimer ( function (zomb, rot) if ( isElement ( zomb ) ) then setPedRotation ( zomb, rot ) end end, 500, 1, zomb, rot )
+				local zskin = randomZskin
+				chaseanim = "run_old"
+				checkspeed = 1000
+				for k, skin in pairs( slowZombies ) do
+					if skin == zskin then
+						chaseanim = "WALK_drunk"
+						checkspeed = 2000
+					end
+				end
+				for k, skin in pairs( fastZombies ) do
+					if skin == zskin then
+						chaseanim = "Run_Wuzi"
+						checkspeed = 680
+					end
+				end
 				setTimer ( function (zomb) if ( isElement ( zomb ) ) then setPedAnimation ( zomb, "ped", chaseanim, -1, true, true, true ) end end, 1000, 1, zomb )
 				setTimer ( function (zomb) if ( isElement ( zomb ) ) then setElementData ( zomb, "status", "idle" ) end end, 2000, 1, zomb )
 				triggerClientEvent ( "Zomb_STFU", getRootElement(), zomb )
